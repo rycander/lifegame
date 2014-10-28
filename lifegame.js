2a17a93184debc6108bdf5c3b9075f764ae86c7b
Ryan Anderson <esrever.nayr@gmail.com>
2014-10-28 16:25:20 -0700

    load functionality, added buttons, styled page, various visual improvements.

--- lifegame.js
+++ lifegame.js
@@ -10,14 +10,6 @@
 
     this.draw();
   };
-  
-  LifeGame.prototype.run = function () {
-    this.running = true;
-  };
-
-  LifeGame.prototype.pause = function () {
-    this.running = false;
-  };
 
   LifeGame.prototype.update = function () {
     this.newCells = {};
@@ -93,9 +85,100 @@
   LifeGame.prototype.draw = function () {
     var ctx = this.ctx;
     ctx.clearRect(0, 0, this.width, this.height);
+    ctx.fillStyle = "#EEEEEE";
+    ctx.fillRect(0, 0, this.width, this.height);
     $.each(this.liveCells, function(cell) {
       this.drawCell(cell);
     }.bind(this));
+    this.drawLines();
+  };
+
+  LifeGame.prototype.drawLines = function () {
+    ctx.fillStyle = "#FFFFFF";
+    for (var i = 0; i < 101; ++i) {
+      ctx.fillRect(i * 8 - 1, 0, 2, 800);
+      ctx.fillRect(0, i * 8 - 1, 800, 2);
+    }
+  };
+
+  LifeGame.prototype.randomColor = function () {
+    return Math.floor(Math.random() * 155);
+  };
+
+  LifeGame.prototype.drawCell = function (cell) {
+    var x = this.liveCells[cell].x;
+    var y = this.liveCells[cell].y;
+    if (
+      x.lessThan(100) &&
+      x.greaterThanOrEqualTo(0) &&
+      y.lessThan(100) &&
+      y.greaterThanOrEqualTo(0)
+    ){
+      ctx.fillStyle = "rgb(" + this.randomColor() + 
+        "," + this.randomColor() +
+        "," + 255 + ")";
+      ctx.fillRect(x*8, y*8, 8, 8);
+    }
+  };
+
+  LifeGame.prototype.createListeners = function () {
+    window.setInterval((function () {
+      if (this.running) {
+        this.update();
+        this.draw(this.ctx);
+      }
+    }).bind(this), 0);
+
+    $("body").keydown( function (event) {
+      if (event.which === 80){
+        this.toggleRun();
+      }
+      if (event.which === 83){
+        this.save();
+      }
+      // if (event.which === 76){
+      //   this.load();
+      // }
+    }.bind(this));
+
+    canvas.addEventListener(
+      'mousedown',
+      function(evt) {
+        var mousePos = this.getMousePos(canvas, evt);
+        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+        this.toggleCell(mousePos.x, mousePos.y);
+      }.bind(this)
+    );
+
+    document.getElementById('save').addEventListener(
+      'click',
+      function (evt) {
+        this.save();
+      }.bind(this),
+      false
+    );
+
+    document.getElementById('play').addEventListener(
+      'click',
+      function (evt) {
+        this.toggleRun();
+      }.bind(this),
+      false
+    );
+
+    document.getElementById('file').addEventListener(
+      'change', function (evt) {
+        this.handleFileSelect(evt);
+      }.bind(this), false
+    );
+  };
+
+  LifeGame.prototype.getMousePos = function (canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: new BigNumber(Math.floor((evt.clientX - rect.left) / 8)),
+      y: new BigNumber(Math.floor((evt.clientY - rect.top) / 8))
+    };
   };
 
   LifeGame.prototype.toggleCell = function (x, y) {
@@ -110,11 +193,20 @@
     this.draw();
   };
 
-  LifeGame.prototype.drawCell = function (cell) {
-    var x = this.liveCells[cell].x;
-    var y = this.liveCells[cell].y;
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(x*5, y*5, 5, 5);
+  LifeGame.prototype.toggleRun = function () {
+    this.running = !this.running;
+  };
+
+  LifeGame.prototype.handleFileSelect = function (evt) {
+    var file = evt.target.files[0];
+    
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = (function() {
+        this.load(reader.result);
+      }.bind(this));
+      reader.readAsText(file);
+    }
   };
 
   LifeGame.prototype.save = function () {
@@ -124,66 +216,33 @@
     window.focus();
   };
 
-  LifeGame.prototype.load = function () {
-
+  LifeGame.prototype.load = function (text) {
+    console.log('loading!');
+    try{
+      var tempCells = JSON.parse(text);
+      this.liveCells = this.convertCellsFromJSON(tempCells);
+      this.draw();
+    }
+    catch(err){
+      console.log(err);
+      alert('Could not parse file.');
+    }
   };
 
-  LifeGame.prototype.createListeners = function () {
-    window.setInterval((function () {
-      if (this.running) {
-        this.update();
-        this.draw(this.ctx);
-      }
-    }).bind(this), 100);
+  LifeGame.prototype.convertToBigNumber = function (num) {
+    var result = new BigNumber (0);
+    result.s = num.s;
+    result.e = num.e;
+    result.c = num.c;
+    return result;
+  };
 
-    $("body").keydown( function (event) {
-      if (event.which === 82){
-        this.run();
-      }
-      if (event.which === 80){
-        this.pause();
-      }
-      if (event.which === 83){
-        this.save();
-      }
-      if (event.which === 76){
-        this.load();
-      }
+  LifeGame.prototype.convertCellsFromJSON = function (cells) {
+    $.each(cells, function(cell) {
+      cells[cell].x = this.convertToBigNumber(cells[cell].x);
+      cells[cell].y = this.convertToBigNumber(cells[cell].y);
     }.bind(this));
-
-    canvas.addEventListener(
-      'mousedown',
-      function(evt) {
-        var mousePos = this.getMousePos(canvas, evt);
-        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-        this.toggleCell(mousePos.x, mousePos.y);
-      }.bind(this));
+    return cells;
   };
-
-  LifeGame.prototype.getMousePos = function (canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: new BigNumber(Math.floor((evt.clientX - rect.left) / 5)),
-      y: new BigNumber(Math.floor((evt.clientY - rect.top) / 5))
-    };
-  };
-    //todo: startListener
-    //todo: clickListener
-  /*var GameView = window.Asteroids.GameView = function(opts) {
-    this.game = new window.Asteroids.Game({
-      height: opts.height,
-      width: opts.width,
-      numAsteroids: Math.ceil(opts.height * opts.width / 25000)
-    });
-    this.ctx = opts.ctx;
-  };
-  
-  GameView.prototype.start = function () {
-    window.setInterval((function () {
-      this.game.update();
-      this.game.draw(this.ctx);
-    }).bind(this), 10);
-  };*/
-  
   
 })();
